05ac293b8c24c252b5600c8a343823790566fb75
sotonalamgir <sotonalamgir01@gmail.com>
2026-07-29 22:13:26 +0600

    1st done

---
+++ 1.js
@@ -0,0 +1,16 @@
+function matchWinner(teamAGoals, teamBGoals) {
+    if ((typeof teamAGoals=="string")||(typeof teamBGoals=="string")){
+        return "Invalid";
+    }else if((teamAGoals===null)||(teamBGoals===null)){
+        return "Invalid";
+    }
+    else if(teamAGoals>teamBGoals){
+    return "Team A Won";
+    }
+    else if(teamBGoals>teamAGoals){
+    return "Team B Won";
+    }
+    else{
+    return "Draw";
+    }
+}
