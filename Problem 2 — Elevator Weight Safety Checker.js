b38bcf27670859e511021b0cded86584a4679e15
sotonalamgir <sotonalamgir01@gmail.com>
2026-07-30 08:21:55 +0600

    done

--- Problem 2 — Elevator Weight Safety Checker.js
+++ Problem 2 — Elevator Weight Safety Checker.js
@@ -1,3 +1,19 @@
+//Problem-02: Elevator Weight Safety Checker
 function isElevatorSafe(weights) {
-  // Write your code here
+  let sum =0;
+  let arryCheck = Array.isArray(weights);
+  if(arryCheck===false){
+    return "Invalid";
+  }
+  for(let weight of weights){
+    sum = sum+weight;
+  }
+  if(sum<=400){
+    return true;
+  }else{
+    return false;
+  }
 }
+
+
+
---
+++ ex.js
@@ -0,0 +1,6 @@
+let maths =[90,88,99];
+let sum=0;
+for(add of maths){
+  sum=sum+add;
+}
+console.log(sum);
