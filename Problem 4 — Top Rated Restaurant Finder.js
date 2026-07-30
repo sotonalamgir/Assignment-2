44eee57ff93be5df60f8b323ecca61e92ca82f62
sotonalamgir <sotonalamgir01@gmail.com>
2026-07-30 20:42:05 +0600

    not solved yet

---
+++ Problem 4 — Top Rated Restaurant Finder.js
@@ -0,0 +1,13 @@
+function topRatedRestaurant(restaurants) {
+    let res = Array.isArray(restaurants);
+    if (res = false || restaurants.length === 0) {
+        return "Invalid";
+    }
+    let topRestaurant = restaurants[0];
+    for (let i = 1; i < restaurants.length; i++) {
+        if (restaurants[i].rating > topRestaurant.rating) {
+            topRestaurant = restaurants[i];
+        }
+    }
+    return topRestaurant.name.toUpperCase();
+}
