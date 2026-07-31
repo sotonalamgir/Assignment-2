801debabe4ebb67dd6835fd7a128c38e71321f4e
sotonalamgir <sotonalamgir01@gmail.com>
2026-07-31 12:02:40 +0600

    done

--- Problem 4 — Top Rated Restaurant Finder.js
+++ Problem 4 — Top Rated Restaurant Finder.js
@@ -1,10 +1,9 @@
 function topRatedRestaurant(restaurants) {
-    let res = Array.isArray(restaurants);
-    if (res = false || restaurants.length === 0) {
+     if (!Array.isArray(restaurants) || restaurants.length === 0) {
         return "Invalid";
     }
     let topRestaurant = restaurants[0];
-    for (let i = 1; i < restaurants.length; i++) {
+    for (let i=1; i<restaurants.length; i++) {
         if (restaurants[i].rating > topRestaurant.rating) {
             topRestaurant = restaurants[i];
         }
